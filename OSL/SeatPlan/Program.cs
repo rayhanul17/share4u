f41e6821f7d80d7f09908bdcc7f852e0693fe081
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-10-08 18:26:26 +0600

    seat plan

---
+++ OSL/SeatPlan/Program.cs
@@ -0,0 +1,77 @@
+﻿var listA = new List<int>{
+    1,2,3,4,5,6,7,8,9,23,24,25,26,27
+};
+var listB = new List<int>{
+    11,22,33,44,55,66,77,88,99
+};
+
+var listACount = listA.Count;
+var listBCount = listB.Count;
+
+List<(List<int>, List<int>)> plans = new();
+
+var colBenchList = new List<int> { 2, 3, 3, 3, 4 };
+
+var skip = 0;
+foreach (var colBench in colBenchList)
+{
+    var bsA = listA.Skip(skip).Take(colBench).ToList();
+    var bsB = listB.Skip(skip).Take(colBench).ToList();
+
+    skip += colBench;
+
+    if (listACount < skip && listBCount > skip)
+    {
+        if (bsA == null)
+        {
+            bsA = new List<int>();
+        }
+
+        var bsAEmptyCount = colBench - bsA.Count;
+        //skip += bsAEmptyCount;
+        bsA.AddRange(listB.Skip(skip).Take(bsAEmptyCount).ToList());
+        skip += bsAEmptyCount;
+    }
+    else
+    {
+        if (bsA == null)
+        {
+            bsA = new List<int>();
+        }
+
+        var bsAEmptyCount = colBench - bsA.Count;
+        for (int i = 0; i < bsAEmptyCount; i++)
+        {
+            bsA.Add(0);
+        }
+    }
+
+    if (listBCount < skip && listACount > skip)
+    {
+        if (bsB == null)
+        {
+            bsB = new List<int>();
+        }
+
+        var bsBEmtyCount = colBench - bsB.Count;
+        bsB.AddRange(listA.Skip(skip).Take(bsBEmtyCount).ToList());
+        skip += bsBEmtyCount;
+    }
+    else
+    {
+        if (bsB == null)
+        {
+            bsB = new List<int>();
+        }
+
+        var bsBEmptyCount = colBench - bsB.Count;
+        for (int i = 0; i < bsBEmptyCount; i++)
+        {
+            bsB.Add(0);
+        }
+    }
+
+    plans.Add((bsA, bsB));
+}
+
+Console.ReadKey();
---
+++ OSL/newSeatPlan/Program.cs
@@ -0,0 +1,76 @@
+﻿var listA = Enumerable.Range(1, 2).ToList();
+var listB = Enumerable.Range(101, 20).ToList();
+
+listA.Reverse();
+listB.Reverse();
+
+var stackA = new Stack<int>(listA);
+var stackB = new Stack<int>(listB);
+
+var colBenchList = new List<int> { 2, 3, 4, 3, 4 };
+var row = colBenchList.Count();
+int[][,] jaggedArray = new int[row][,];
+
+int spb = 0;
+
+Console.Write("Student Per Brench: ");
+int.TryParse(Console.ReadLine(), out spb);
+
+Console.Write("Zigzag type: ");
+string zigzag = Console.ReadLine();
+
+bool isZigzag = string.IsNullOrWhiteSpace(zigzag) ? false : zigzag == "0" ? false : true;
+bool toggle = false;
+int studentPerBrench = spb;
+
+
+for (int i = 0; i < jaggedArray.GetLength(0); i++)
+{
+    jaggedArray[i] = new int[colBenchList[i], studentPerBrench];
+    
+    for (int j = 0; j < colBenchList[i]; j++)
+    {
+        for (int k = 0; k < studentPerBrench; k++)
+        {
+            if (isZigzag)
+            {
+                if ((i + j + k) % 2 == 0)
+                    stackA.TryPop(out jaggedArray[i][j, k]);
+                else
+                    stackB.TryPop(out jaggedArray[i][j, k]);
+            }
+            else
+            {
+                if (k % 2 == 0)
+                    stackA.TryPop(out jaggedArray[i][j, k]);
+                else
+                    stackB.TryPop(out jaggedArray[i][j, k]);
+            }
+            
+        }        
+    } 
+}
+
+/*          if (k % 2 == 0)
+                stackA.TryPop(out jaggedArray[i][j, k]);
+            else
+               stackB.TryPop(out jaggedArray[i][j, k]);
+//Hard:1
+            if ((i + j + k) % 2 == 0)
+                stackA.TryPop(out jaggedArray[i][j, k]);                
+            else
+                stackB.TryPop(out jaggedArray[i][j, k]); 
+//Hard:2 
+*/
+for (int i = 0; i < jaggedArray.GetLength(0); i++)
+{
+    Console.WriteLine($"Column {i + 1}");
+    for (int j = 0; j < jaggedArray[i].GetLength(0); j++)
+    {
+        for (int k = 0; k < jaggedArray[i].GetLength(1); k++)
+            Console.Write($"[{i}][{j},{k}] = {jaggedArray[i][j, k]}\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
